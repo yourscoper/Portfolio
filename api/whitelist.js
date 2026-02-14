95ecde38fd46cad824b8ac7eca2efa446635d7a3
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-02-14 13:36:37 -0800

    Update whitelist.js

--- api/whitelist.js
+++ api/whitelist.js
@@ -1,10 +1,8 @@
 const whitelistData = {
   "whitelist": {
-    "product": {
-      "discord": "765706304656113664",
-      "key": "5wn1Ger8HaWutECX9BLwQtLl1i1h1I",
-      "hwid": "f0a9890c96a38a70555322fad30a7582071c953dc3f4f8a93f5c05377e8d3daf7e3feac042caada1e5595b91c54e86d1378ef2ece37598278298d90eaaacff44"
-    }
+    "discord": "765706304656113664",
+    "key": "5wn1Ger8HaWutECX9BLwQtLl1i1h1I",
+    "hwid": "f0a9890c96a38a70555322fad30a7582071c953dc3f4f8a93f5c05377e8d3daf7e3feac042caada1e5595b91c54e86d1378ef2ece37598278298d90eaaacff44"
   }
 };
 
