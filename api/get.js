62ecce650791bd5e2fedb937644f4aead489188a
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-03-21 08:38:47 -0700

    Update get.js

--- api/get.js
+++ api/get.js
@@ -130,7 +130,6 @@ export default function handler(req, res) {
   const response = {
     args: req.query || {},
     headers: outputHeaders,
-    "x-amzn-trace-id": headers["x-amzn-trace-id"] || "not present",
     location,
     origin: location.ip,
     url: `https://${headers["host"]}${req.url}`
