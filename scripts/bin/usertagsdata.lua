2e81d44bd21c4bc75b68586eb1b329e1daa95955
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-04-25 18:37:05 -0700

    Update usertagsdata.lua

--- scripts/bin/usertagsdata.lua
+++ scripts/bin/usertagsdata.lua
@@ -1,6 +1,7 @@
 return {
     TagOrder = {
         "OWNER"
+        "UNICORN MAN"
     },
   
     RankData = {
@@ -8,11 +9,22 @@ return {
             primary = Color3.fromRGB(255, 255, 255),
             useimage = true,
             accent = ColorSequence.new{
-                ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 0, 0)),
-                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(75, 255, 125)),
-                ColorSequenceKeypoint.new(1, Color3.fromRGB(50, 255, 255)),
+                ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 230, 70)),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(110, 101, 45)),
+                ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
             },
             image = "http://www.roblox.com/asset/?id=10885640682"
+        },
+        
+        ["UNICORN MAN"] = {
+            primary = Color3.fromRGB(255, 255, 255),
+            useimage = true,
+            accent = ColorSequence.new{
+                ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 30, 255)),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(135, 22, 135)),
+                ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
+            },
+            image = "http://www.roblox.com/asset/?id=128445732"
         },
     }
 }
