035609149e745fe1c8a5d44aafd65e08e341a81c
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-04-26 17:19:32 -0700

    Update usertagsdata.lua

--- scripts/bin/usertagsdata.lua
+++ scripts/bin/usertagsdata.lua
@@ -7,36 +7,47 @@ return {
   
     RankData = {
         ["OWNER"] = {
-            primary = Color3.fromRGB(255, 255, 255),
-            useimage = true,
-            accent = ColorSequence.new{
-                ColorSequenceKeypoint.new(0, Color3.fromRGB(173, 55, 55)),
-                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(252, 183, 119)),
-                ColorSequenceKeypoint.new(1, Color3.fromRGB(255, 230, 70)),
+            primary     = Color3.fromRGB(255, 255, 255),
+            useimage    = true,
+            accent      = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,    Color3.fromRGB(80, 115, 255)),
+                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(255, 255, 255)),
+                ColorSequenceKeypoint.new(1,    Color3.fromRGB(255, 115, 115)),
             },
-            image = "http://www.roblox.com/asset/?id=7487018714"
+            image       = "http://www.roblox.com/asset/?id=7487018714"
         },
 
         ["ADMIN"] = {
-            primary = Color3.fromRGB(255, 255, 255),
-            useimage = true,
-            accent = ColorSequence.new{
-                ColorSequenceKeypoint.new(0, Color3.fromRGB(250, 95, 92)),
-                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(145, 56, 54)),
-                ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
+            primary     = Color3.fromRGB(255, 255, 255),
+            useimage    = true,
+            accent      = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,    Color3.fromRGB(250, 95, 92)),
+                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(145, 56, 54)),
+                ColorSequenceKeypoint.new(1,    Color3.fromRGB(0, 0, 0)),
             },
-            image = "http://www.roblox.com/asset/?id=7487018714"
+            image       = "http://www.roblox.com/asset/?id=7487018714"
         },
 
         ["UNICORN MAN"] = {
-            primary = Color3.fromRGB(255, 255, 255),
-            useimage = true,
-            accent = ColorSequence.new{
-                ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 30, 255)),
-                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(135, 22, 135)),
-                ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
+            primary     = Color3.fromRGB(255, 255, 255),
+            useimage    = true,
+            accent      = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,    Color3.fromRGB(255, 30, 255)),
+                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(135, 22, 135)),
+                ColorSequenceKeypoint.new(1,    Color3.fromRGB(0, 0, 0)),
             },
-            image = "http://www.roblox.com/asset/?id=90144296400082"
+            image       = "http://www.roblox.com/asset/?id=90144296400082"
+        },
+
+        ["papi angel"] = {
+            primary     = Color3.fromRGB(255, 255, 255),
+            useimage    = true,
+            accent      = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,    Color3.fromRGB(207, 29, 70)),
+                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(112, 29, 207)),
+                ColorSequenceKeypoint.new(1,    Color3.fromRGB(29, 207, 195)),
+            },
+            image       = "http://www.roblox.com/asset/?id=90144296400082"
         },
     }
 }
