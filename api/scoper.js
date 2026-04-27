129a858b260ee4973e126b797336d2814a77a27a
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-04-27 13:36:49 -0700

    Update and rename print.js to scoper.js

--- api/scoper.js
+++ api/scoper.js
@@ -1,15 +1,13 @@
 export default function handler(req, res) {
   const accept = req.headers.accept || "";
 
-  // If it's a browser request → redirect
   if (accept.includes("text/html")) {
     res.writeHead(302, { Location: "/" });
     return res.end();
   }
 
-  // Otherwise → return Lua script
   res.setHeader("Content-Type", "text/plain");
   res.send(`
-print("hello from lua")
+      print("hello from lua")
   `);
 }
