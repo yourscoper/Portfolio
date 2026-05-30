1dc63e1e14607111a81c8b37d6207786a503e5ea
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-05-29 18:02:18 -0700

    Create commandlibrary.js

---
+++ api/commandlibrary.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const accept = req.headers.accept || "";
+
+  if (accept.includes("text/html")) {
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
+
+  const filePath = path.join(process.cwd(), "scripts", "akadmin", "commandlibrary.lua");
+  const script = fs.readFileSync(filePath, "utf8");
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(script);
+}
