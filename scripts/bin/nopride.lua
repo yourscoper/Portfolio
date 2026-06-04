0b6e03ce37c842e31d2f65490b4e531c5356a0a7
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-06-04 15:04:45 -0700

    Update nopride.lua

--- scripts/bin/nopride.lua
+++ scripts/bin/nopride.lua
@@ -6,9 +6,9 @@ local QueueTeleport = queue_on_teleport or queueonteleport
 
 QueueTeleport('loadstring(game:HttpGet("https://yourscoper.vercel.app/nopride.lua"))()')
 
-local map = workspace.map
+local map = workspace:WaitForChild("map", 5)
 
-workspace.Baseplate.Color = Color3.fromRGB(94, 255, 129)
+workspace:WaitForChild("Baseplate", 5).Color = Color3.fromRGB(94, 255, 129)
 
 for _, v in ipairs(map:GetDescendants()) do
 	if v:IsA("BasePart") then
