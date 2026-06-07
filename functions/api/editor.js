9c4bcece48a48c34c7a7cf9145cc7dad98947aaf
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-06-06 20:18:52 -0700

    Add files via upload

---
+++ functions/api/editor.js
@@ -0,0 +1,12 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export default function handler(req, res) {
+  try {
+    const html = readFileSync(join(process.cwd(), 'api/potassiumeditor.html'), 'utf8');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.status(200).send(html);
+  } catch (err) {
+    res.status(500).send('Could not load editor: ' + err.message);
+  }
+}
