cae9b8796fa32242b16b5ce1aa3502732473bb78
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-06-07 14:55:27 -0700

    Delete functions/api directory

--- functions/api/editor.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const { request } = context;
-  const baseUrl = new URL(request.url).origin;
-  const res = await fetch(`${baseUrl}/api/potassiumeditor.html`);
-  if (!res.ok) return new Response("Could not load editor", { status: 500 });
-  const html = await res.text();
-  return new Response(html, { headers: { "Content-Type": "text/html; charset=utf-8" } });
-}
