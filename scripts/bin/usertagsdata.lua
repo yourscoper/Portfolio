9df66ca43263fea5bfc7522e16cc5213d803ec6f
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-06-08 04:51:07 -0700

    Update usertagsdata.lua

--- scripts/bin/usertagsdata.lua
+++ scripts/bin/usertagsdata.lua
@@ -6,55 +6,91 @@ return {
         "papi angel",
         "that nigger gon"
     },
-  
+
     RankData = {
+
         ["OWNER"] = {
-            primary     = Color3.fromRGB(255, 255, 255),
-            useimage    = true,
-            accent      = ColorSequence.new{
-                ColorSequenceKeypoint.new(0,    Color3.fromRGB(0, 0, 0)),
-                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(145, 138, 102)),
-                ColorSequenceKeypoint.new(1,    Color3.fromRGB(240, 210, 60)),
+            primary       = Color3.fromRGB(255, 255, 255),
+            useimage      = true,
+            tageffects    = true,
+            nameeffects   = false,
+            glitchedname  = true,
+            typewritename = false,
+            crownicon     = true,
+            showuserid    = false,
+            tagcolor      = Color3.fromRGB(255, 215, 60),
+            accent        = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,   Color3.fromRGB(0,   0,   0  )),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(145, 138, 102)),
+                ColorSequenceKeypoint.new(1,   Color3.fromRGB(240, 210, 60 )),
             },
-            image       = "http://www.roblox.com/asset/?id=10086464520"
+            image         = "http://www.roblox.com/asset/?id=10086464520"
         },
 
         ["ADMIN"] = {
-            primary     = Color3.fromRGB(255, 255, 255),
-            useimage    = true,
-            accent      = ColorSequence.new{
-                ColorSequenceKeypoint.new(0,    Color3.fromRGB(250, 95, 92)),
-                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(145, 56, 54)),
-                ColorSequenceKeypoint.new(1,    Color3.fromRGB(0, 0, 0)),
+            primary       = Color3.fromRGB(255, 255, 255),
+            useimage      = true,
+            tageffects    = true,
+            nameeffects   = true,
+            glitchedname  = false,
+            typewritename = false,
+            crownicon     = false,
+            showuserid    = false,
+            tagcolor      = Color3.fromRGB(255, 100, 100),
+            accent        = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,   Color3.fromRGB(250, 95,  92 )),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(145, 56,  54 )),
+                ColorSequenceKeypoint.new(1,   Color3.fromRGB(0,   0,   0  )),
             },
-            image       = "http://www.roblox.com/asset/?id=18565602368"
+            image         = "http://www.roblox.com/asset/?id=18565602368"
         },
 
         ["UNICORN MAN"] = {
-            primary     = Color3.fromRGB(255, 255, 255),
-            useimage    = true,
-            accent      = ColorSequence.new{
-                ColorSequenceKeypoint.new(0,    Color3.fromRGB(255, 30, 255)),
-                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(135, 22, 135)),
-                ColorSequenceKeypoint.new(1,    Color3.fromRGB(0, 0, 0)),
+            primary       = Color3.fromRGB(255, 255, 255),
+            useimage      = true,
+            tageffects    = true,
+            nameeffects   = true,
+            glitchedname  = false,
+            typewritename = true,
+            crownicon     = false,
+            showuserid    = false,
+            tagcolor      = Color3.fromRGB(255, 100, 255),
+            accent        = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,   Color3.fromRGB(255, 30,  255)),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(135, 22,  135)),
+                ColorSequenceKeypoint.new(1,   Color3.fromRGB(0,   0,   0  )),
             },
-            image       = "http://www.roblox.com/asset/?id=90144296400082"
+            image         = "http://www.roblox.com/asset/?id=90144296400082"
         },
 
         ["papi angel"] = {
-            primary     = Color3.fromRGB(255, 255, 255),
-            useimage    = true,
-            accent      = ColorSequence.new{
-                ColorSequenceKeypoint.new(0,    Color3.fromRGB(0, 0, 0)),
-                ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(255, 255, 255)),
-                ColorSequenceKeypoint.new(1,    Color3.fromRGB(0, 0, 0)),
+            primary       = Color3.fromRGB(255, 255, 255),
+            useimage      = true,
+            tageffects    = false,
+            nameeffects   = false,
+            glitchedname  = false,
+            typewritename = true,
+            crownicon     = false,
+            showuserid    = false,
+            tagcolor      = Color3.fromRGB(255, 255, 255),
+            accent        = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,   Color3.fromRGB(0,   0,   0  )),
+                ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 255, 255)),
+                ColorSequenceKeypoint.new(1,   Color3.fromRGB(0,   0,   0  )),
             },
-            image       = "http://www.roblox.com/asset/?id=115020167372123"
+            image         = "http://www.roblox.com/asset/?id=115020167372123"
         },
 
         ["that nigger gon"] = {
-            primary     = Color3.fromRGB(255, 255, 255),
-            useimage    = true,
+            primary       = Color3.fromRGB(255, 255, 255),
+            useimage      = true,
+            tageffects    = false,
+            nameeffects   = false,
+            glitchedname  = false,
+            typewritename = true,
+            crownicon     = false,
+            showuserid    = false,
+            tagcolor      = Color3.fromRGB(255, 255, 255),
             accent      = ColorSequence.new{
                 ColorSequenceKeypoint.new(0,    Color3.fromRGB(58, 2, 61)),
                 ColorSequenceKeypoint.new(0.5,  Color3.fromRGB(68, 43, 69)),
@@ -62,5 +98,6 @@ return {
             },
             image       = "http://www.roblox.com/asset/?id=132854398900146"
         }
+
     }
 }
